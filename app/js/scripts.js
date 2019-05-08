3c1caee6ebaf6846c1a5fed6b27334649b219b11
Yur4ik123 <yur.malishko@gmail.com>
2019-05-09 00:54:09 +0300

    Переделал табы на bootstrap

---
+++ app/js/scripts.js
@@ -0,0 +1,36 @@
+
+$(function () {
+  $("#tabs-script").tabs();
+  $('.multiple-items').slick({
+    infinite: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+          breakpoint: 1640,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 850,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+           
+          }
+        }
+    ]
+  });
+});
