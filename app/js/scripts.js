78e640b49739c52aa50b7bb9fccf103842d74182
Yur4ik123 <yur.malishko@gmail.com>
2019-05-10 13:43:13 +0300

    update

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -33,4 +33,13 @@ $(function () {
         }
     ]
   });
+  $(".js-range-slider").ionRangeSlider({
+    skin: "round",
+    type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+});
 });
