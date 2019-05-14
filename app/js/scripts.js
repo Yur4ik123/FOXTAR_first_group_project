f86695f77efe15fbd867f67ad18eb984c87fa761
Yur4ik123 <yur.malishko@gmail.com>
2019-05-14 18:52:43 +0300

    Исправлена остановка скрипта ionSlider

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -33,13 +33,5 @@ $(function () {
         }
     ]
   });
-  $(".js-range-slider").ionRangeSlider({
-    skin: "round",
-    type: "double",
-        min: 0,
-        max: 1000,
-        from: 0,
-        to: 600,
-        prefix: "$"
-});
+ 
 });
